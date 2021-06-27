98fc6640da0e838a67e541f29d50963c2a5d9219
Chrishan Jayakody <chrishan@cinnamonit.com>
2021-06-27 09:06:27 +0530

    Added dynamic stackview

--- StackViewDemo/ViewController.swift
+++ StackViewDemo/ViewController.swift
@@ -8,11 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func onAddClick(_ sender: UIButton) {
+        let button = UIButton(frame: sender.frame)
+        button.backgroundColor = .blue
+        button.setTitle("Button", for: .normal)
+        
+        stackView.addArrangedSubview(button)
+    }
+    
 
 
 }
