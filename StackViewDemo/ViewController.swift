32d34b19c8d916d6334aaa61e2fa765b55662f54
Chrishan Jayakody <chrishan@cinnamonit.com>
2021-06-27 09:46:52 +0530

    Add remove function

--- StackViewDemo/ViewController.swift
+++ StackViewDemo/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    static let colours = [
+        UIColor.blue,
+        UIColor.black,
+        UIColor.purple,
+        UIColor.systemGreen,
+        UIColor.red,
+        UIColor.darkGray
+    ]
+    
     @IBOutlet weak var stackView: UIStackView!
     
     override func viewDidLoad() {
@@ -18,13 +27,21 @@ class ViewController: UIViewController {
     
     @IBAction func onAddClick(_ sender: UIButton) {
         let button = UIButton(frame: sender.frame)
-        button.backgroundColor = .blue
+        button.backgroundColor = ViewController.colours.randomElement()
         button.setTitle("Button", for: .normal)
         
         stackView.addArrangedSubview(button)
     }
     
-
+    @IBAction func onRemoveClicked(_ sender: Any) {
+        if stackView.arrangedSubviews.count == 0 {
+            return
+        }
+        
+        let view = stackView.arrangedSubviews[stackView.arrangedSubviews.count - 1]
+        view.removeFromSuperview()
+    }
+    
 
 }
 
