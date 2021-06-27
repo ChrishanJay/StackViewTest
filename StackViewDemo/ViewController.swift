5e64a67527a8b01f6408c9ba117e33cb482bb285
Chrishan Jayakody <chrishan@cinnamonit.com>
2021-06-27 07:04:45 +0530

    Initial Commit

---
+++ StackViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by Chrishan Jayakody on 6/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
